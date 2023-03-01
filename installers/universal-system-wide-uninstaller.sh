9b64c95a7604e477471aa6667b53a31bd79b14f4
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-03-01 09:40:03 -0600

    Create universal-system-wide-uninstaller.sh

---
+++ installers/universal-system-wide-uninstaller.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Uninstalling nix-portable..."
+sudo rm /usr/local/bin/nix-portable
+sudo rm /usr/local/bin/nix
+sudo rm /usr/local/bin/nix-shell
+sudo rm /usr/local/bin/nix-env
+sudo rm /usr/local/bin/nix-store
+sudo rm /usr/local/bin/nix-build
+sudo rm /usr/local/bin/nix-channel
+sudo rm /usr/local/bin/nix-collect-garbage
+sudo rm /usr/local/bin/nix-copy-closure
+sudo rm /usr/local/bin/nix-hash
+sudo rm /usr/local/bin/nix-instantiate
+sudo rm /usr/local/bin/nix-prefetch-url
