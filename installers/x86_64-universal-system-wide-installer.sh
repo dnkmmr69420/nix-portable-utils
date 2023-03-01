8ec1993d0f72e968650c5c443ac7b0511a3ae7c4
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-03-01 09:26:50 -0600

    Update x86_64-universal-system-wide-installer.sh

--- installers/x86_64-universal-system-wide-installer.sh
+++ installers/x86_64-universal-system-wide-installer.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 sudo wget -P /usr/local/bin https://github.com/DavHau/nix-portable/releases/download/v009/nix-portable
 sudo chmod a+x /usr/local/bin/nix-portable
-sudo ln -s /usr/local/bin/nix-portable /usr/local/bin/nix
-sudo ln -s /usr/local/bin/nix-portable /usr/local/bin/nix-shell
-sudo ln -s /usr/local/bin
+
+
+while true
+do
+      read -r -p "Do you want to symlink nix, nix-env, nix-shell against nix-portable? [Y/n] " input
+ 
+      case $input in
+            [yY][eE][sS]|[yY])
+                  echo "Creating symlinks against nix-portable"
+                  sudo ln -s /usr/local/bin/nix-portable /usr/local/bin/nix
+                  sudo ln -s /usr/local/bin/nix-portable /usr/local/bin/nix-shell
+                  sudo ln -s /usr/local/bin/nix-portable /usr/local/bin/nix-env
+                  sudo ln -s /usr/local/bin/nix-portable /usr/local/bin/nix-store
+                  sudo ln -s /usr/local/bin/nix-portable /usr/local/bin/nix-build
+                  sudo ln -s /usr/local/bin/nix-portable /usr/local/bin/nix-channel
+                  sudo ln -s /usr/local/bin/nix-portable /usr/local/bin/nix-collect-garbage
+                  sudo ln -s /usr/local/bin/nix-portable /usr/local/bin/nix-copy-closure
+                  sudo ln -s /usr/local/bin/nix-portable /usr/local/bin/nix-hash
+                  sudo ln -s /usr/local/bin/nix-portable /usr/local/bin/nix-instantiate
+                  sudo ln -s /usr/local/bin/nix-portable /usr/local/bin/nix-prefetch-url
+                  break
+                  ;;
+            [nN][oO]|[nN])
+                  echo "Skipping symlink creation"
+                  break
+                  ;;
+            *)
+                  echo "Invalid input..."
+                  ;;
+      esac      
+done
