287389f675fe398102c05b89c931d9358e92af8c
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-03-02 09:40:36 -0600

    Create system-wide-installer.sh

---
+++ nix-exec-and-ls/system-wide-installer.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo wget -P /usr/local/bin https://raw.githubusercontent.com/dnkmmr69420/nix-portable-utils/main/nix-exec-and-ls/nix-exec
+sudo wget -P /usr/local/bin https://raw.githubusercontent.com/dnkmmr69420/nix-portable-utils/main/nix-exec-and-ls/nix-ls
+sudo chmod a+x /usr/local/bin/nix-exec
+sudo chmod a+x /usr/local/bin/nix-ls
