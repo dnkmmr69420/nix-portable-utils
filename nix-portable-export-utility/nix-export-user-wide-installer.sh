599fb8b360ee0c8d7a8e8a94f83acb6971c11626
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-03-02 13:12:17 -0600

    Update nix-export-user-wide-installer.sh

--- nix-portable-export-utility/nix-export-user-wide-installer.sh
+++ nix-portable-export-utility/nix-export-user-wide-installer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm ~/.local/bin/nix-export
 wget -P ~/.local/bin https://raw.githubusercontent.com/dnkmmr69420/nix-portable-utils/main/nix-portable-export-utility/nix-export
-sudo chmod a+x ~/.local/bin/nix-export
+chmod a+x ~/.local/bin/nix-export
