667190e61d657c52ff453927582b4c1b73101389
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-03-02 09:46:51 -0600

    Update system-wide-installer.sh

--- nix-exec-and-ls/system-wide-installer.sh
+++ nix-exec-and-ls/system-wide-installer.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo rm /usr/local/bin/nix-exec
+sudo rm /usr/local/bin/nix-ls
 sudo wget -P /usr/local/bin https://raw.githubusercontent.com/dnkmmr69420/nix-portable-utils/main/nix-exec-and-ls/nix-exec
 sudo wget -P /usr/local/bin https://raw.githubusercontent.com/dnkmmr69420/nix-portable-utils/main/nix-exec-and-ls/nix-ls
 sudo chmod a+x /usr/local/bin/nix-exec
