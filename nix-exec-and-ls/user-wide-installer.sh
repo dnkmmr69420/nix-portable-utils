72f5d3077565b9f3b1b73fe2ab95a5b2efbed56c
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-03-02 09:48:02 -0600

    Update user-wide-installer.sh

--- nix-exec-and-ls/user-wide-installer.sh
+++ nix-exec-and-ls/user-wide-installer.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+rm ~/.local/bin/nix-exec
+rm ~/.local/bin/nix-ls
 wget -P ~/.local/bin https://raw.githubusercontent.com/dnkmmr69420/nix-portable-utils/main/nix-exec-and-ls/nix-exec
 wget -P ~/.local/bin https://raw.githubusercontent.com/dnkmmr69420/nix-portable-utils/main/nix-exec-and-ls/nix-ls
 chmod a+x ~/.local/bin/nix-exec
