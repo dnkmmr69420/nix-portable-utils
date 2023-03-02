fab5c54cf384d123db9654377cd77825c8b72776
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-03-02 09:44:09 -0600

    Create user-wide-installer.sh

---
+++ nix-exec-and-ls/user-wide-installer.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget -P ~/.local/bin https://raw.githubusercontent.com/dnkmmr69420/nix-portable-utils/main/nix-exec-and-ls/nix-exec
+wget -P ~/.local/bin https://raw.githubusercontent.com/dnkmmr69420/nix-portable-utils/main/nix-exec-and-ls/nix-ls
+chmod a+x ~/.local/bin/nix-exec
+chmod a+x ~/.local/bin/nix-ls
