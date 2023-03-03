ad8547f6011c3bc1c195db52a07e1d4e5d5a1346
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-03-03 09:32:30 -0600

    Delete user-wide-installer.sh

--- nix-alias/user-wide-installer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm ~/.local/bin/nix-alias
-rm ~/.local/bin/nix-abalias
-wget -p ~/.local/bin https://raw.githubusercontent.com/dnkmmr69420/nix-portable-utils/main/nix-alias/nix-alias
-wget -p ~/.local/bin https://raw.githubusercontent.com/dnkmmr69420/nix-portable-utils/main/nix-alias/nix-abalias
-chmod a+x ~/.local/bin/nix-alias
-chmod a+x ~/.local/bin/nix-abalias
-echo "source ~/.config/nix-alias" >> ~/.bashrc
