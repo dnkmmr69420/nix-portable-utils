3bfa31ed419714451c8b602ee565af1e1a0d9086
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-03-03 10:26:42 -0600

    Create nix-alias-system-wide-uninstaller.sh

---
+++ nix-alias/nix-alias-system-wide-uninstaller.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo echo "Uninstalling nix-alias"
+sudo rm /usr/local/bin/nix-alias
+sudo rm /usr/local/bin/nix-abalias
+sudo rm /usr/local/bin/nix-alias-setup
+sudo echo "Uninstalled nix-alias"
