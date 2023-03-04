a210c3e10d8cfb02a113ac1a2f42811bf244a1f4
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-03-04 08:08:46 -0600

    Update nix-setup-system-wide-installer.sh

--- nix-setup/nix-setup-system-wide-installer.sh
+++ nix-setup/nix-setup-system-wide-installer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo rm /usr/local/bin/nix-setup
-sudo wget -P /usr/local/bin https://raw.githubusercontent.com/dnkmmr69420/nix-portable-utils/main/nix-alias/nix-alias-system-wide-installer.sh
+sudo wget -P /usr/local/bin https://raw.githubusercontent.com/dnkmmr69420/nix-portable-utils/main/nix-setup/nix-setup
 sudo chmod a+x /usr/local/bin/nix-setup
